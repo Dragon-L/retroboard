5f8830e250f48a790a27e55e79c6580084716b2d
Kai Hu <kwrongkey@gmail.com>
2016-12-22 16:29:29 +0800

    [Kai] init project.

---
+++ src/main/java/com/tw/retroboard/RetroboardApplication.java
@@ -0,0 +1,12 @@
+package com.tw.retroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroboardApplication.class, args);
+	}
+}
